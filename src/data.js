e865784f47e1656aa9c7fd0970fa2eea112e18e8
OmkarGore19 <omkaragore19@gmail.com>
2024-05-18 17:20:26 +0530

    initial message

---
+++ src/data.js
@@ -0,0 +1,62 @@
+export const projects = [
+  {
+    title: "Urban Spices - Online Food Delivery App",
+    subtitle: "React & TailwindCSS",
+    description:
+      "Urban Spices - Online Food Delivery App is a ReactJS-based project developed with Tailwind CSS, offering a modern and intuitive platform for ordering food online.",
+    image: "./project1.png",
+    link: "https://omkargore19.github.io/Urban-Spices-React-App/",
+  },
+  {
+    title: "Disney+Hotstar Home Page Clone",
+    subtitle: "HTML & CSS",
+    description:
+      "Disney+Hotstar Home Page Clone is a mini project crafted using HTML & CSS. This project emulates the visually appealing layout of the Disney+Hotstar home page,a popular streaming platform.",
+    image: "./project2.png",
+    link: "https://github.com/OmkarGore19/Disney-Hotsar-Home-Page-Clone",
+  },
+  {
+    title: "DataFin. - A Data Finance App",
+    subtitle: "React & TailwindCSS",
+    description:
+      "DataFin. is a next-generation Data Finance application crafted using React and TailwindCSS.",
+    image: "./project3.png",
+    link: "https://react-project-data-fin.vercel.app/",
+  },
+  {
+    title: "To-do List App",
+    subtitle: "React & React Bootstrap",
+    description:
+      "The To-do List App is a versatile task management solution built using React and React Bootstrap. ",
+    image: "./project4.png",
+    link: "https://react-todo-app-v5.vercel.app/",
+  },
+];
+
+export const testimonials = [
+  {
+    quote:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa quod necessitatibus quidem impedit facilis? Distinctio laboriosam veritatis recusandae minus. Voluptatum accusantium hic laudantium soluta ea sapiente cupiditate officia maiores itaque.",
+    image: "https://randomuser.me/api/portraits/men/1.jpg",
+    name: "Jesse Hicks",
+    company: "Zoozle",
+  },
+  {
+    quote:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa quod necessitatibus quidem impedit facilis? Distinctio laboriosam veritatis recusandae minus. Voluptatum accusantium hic laudantium soluta ea sapiente cupiditate officia maiores itaque.",
+    image: "https://randomuser.me/api/portraits/men/94.jpg",
+    name: "Ruben Alvarez",
+    company: "Dooble.io",
+  },
+];
+
+export const skills = [
+  "HTML",
+  "CSS",
+  "TailwindCSS",
+  "JavaScript",
+  "React",
+  "Node",
+  "MondoDB",
+  "MySQL",
+];
